81f7805c0e84cee74719bfa8942d374534c91579
Edward Taylor <ted@greenfish.net>
2023-08-03 16:37:32 +1000

    added cron job

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { SapphireClient } from '@sapphire/framework';
-import { GatewayIntentBits } from 'discord.js';
+import { GatewayIntentBits, TextChannel } from 'discord.js';
+import { CronJob } from 'cron';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -7,4 +8,13 @@ const client = new SapphireClient({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
+const job = new CronJob('0 17 * * *', () => {
+    const channel = client.channels.cache.get('1136523488560160778')!;
+    if (!(channel instanceof TextChannel)) return;
+    channel.messages.cache.forEach(message => {
+        message.delete();
+    })
+})
+
+job.start();
 client.login(process.env.TOKEN);
